de3e230653a7a02712d4c5ef07fcf8b900681fae
ap6430219 <apandey11200@gmail.com>
2025-04-14 09:15:24 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict" // treat all js code as newer virson code
+// alert("Hello World"); // alert box // we are using node js not browser
+// alert("3+3"); // we are using node js not browser
+
+// console.log(3
+// +3); // print in console in this type in incorrect format code run but not in correct format as readable
+// console.log(3 + 3); // print in console in this type in correct format code run and readable
+
+
+console.log("Aditya");
+
+let name = "Aditya";
+let age = 22;
+let isLoggedIn = false;
+let state = null; // no value
+
+
+
+// number => 2 to power 53
+// bigint => 
+// string => ""
+// boolean => true/false
+// null => standlone value
+// undefined => declared but not assigned
+// symbol => unique 
+// object => collection of key value pair
+console.log(typeof age); 
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof "Aditya");
+
+
+

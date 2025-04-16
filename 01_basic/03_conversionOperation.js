53370bd7a2236760eb2d87ca05adabd7531cac2a
ap6430219 <apandey11200@gmail.com>
2025-04-16 13:29:21 +0000

    conversion of data types

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,13 +1,38 @@
 
 // conversion operation
 
-let score = true;
+//let score = true;
 
 
-console.log(typeof score); // number
-console.log(typeof (score));
+//console.log(typeof score); // number
+//console.log(typeof (score));
 
-let valueinNumber = Number(score); // convert string to number
-console.log(typeof valueinNumber); // number
+//let valueinNumber = Number(score); // convert string to number
+//console.log(typeof valueinNumber); // number
 
-console.log(valueinNumber); 
+//console.log(valueinNumber); 
+//"33"=> 33
+//"33abc"=> NaN
+//TRUE=> 1
+//FALSE=> 0
+//"abc"=> NaN
+
+
+
+let isloggedIn = "undefined"; // string
+
+
+
+let booleanisloggedIn = Boolean(isloggedIn); // convert string to boolean
+console.log(booleanisloggedIn); 
+//1= > true
+//0=> false
+//"abc"=> true
+//""=> false
+//"0"=> true
+//"1"=> true
+//"false"=> true
+//"true"=> true
+//"null"=> true
+//"undefined"=> true
+//"NaN"=> true

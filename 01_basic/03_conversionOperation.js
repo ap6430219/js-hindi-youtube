e233622a293dfdcb370fc53c4020b6bb93a42444
ap6430219 <apandey11200@gmail.com>
2025-04-16 09:47:48 +0000

    conversion

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,13 @@
+
+// conversion operation
+
+let score = true;
+
+
+console.log(typeof score); // number
+console.log(typeof (score));
+
+let valueinNumber = Number(score); // convert string to number
+console.log(typeof valueinNumber); // number
+
+console.log(valueinNumber); 
